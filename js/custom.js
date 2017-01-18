9a131b214e6b94fad28a6c1075b3c9a4b49fe32a
slychops <richies75@gmail.com>
2017-01-18 13:49:13 +0100

    Thumbnails
    Setting up thumbnail images

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$("#cat").click(
+
+	function(){
+		console.log($("#cat"));
+		$(".container").html('<img id="cat" src ="https://www.samanthasbell.com/wp-content/uploads/2015/01/dog-drawing-5.jpg">')
+
+}
+);

bd484ce5086729f155023163873457f410755a6d
slychops <richies75@gmail.com>
2017-01-20 15:19:41 +0100

    fiddling with line

--- js/custom.js
+++ js/custom.js
@@ -20,7 +20,9 @@
 var line1 = $('#line1');
 var div1 = $('#one-but');
 var div2 = $('.profilePic');
-var pos1 = div1.position();
-var pos2 = div2.position();
+// var div3 = $('.mainMenu');
+var pos1 = div1.offset();
+var pos2 = div2.offset();
+// var pos3 = 
 
-line1.attr('x1',pos1.left).attr('y1',pos1.top).attr('x2',pos2.left).attr('y2',pos2.top);
+line1.attr('x1',pos1.left + div1.width()/2).attr('y1',pos1.top + div1.height()/2).attr('x2',pos2.left + div2.width()/2).attr('y2',pos2.top + div2.height()/2);
